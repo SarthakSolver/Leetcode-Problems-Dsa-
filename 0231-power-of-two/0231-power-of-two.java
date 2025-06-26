4d55bbaefeb00942d320ee3127de3eee513bfa69
SarthakSolver <148975170+SarthakSolver@users.noreply.github.com>
2025-06-26 12:54:10 +0530

    Time: 0 ms (100%), Space: 40.8 MB (79.6%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,12 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==1)
-        return true;
-        if(n<=0||n%2==1){
-            return false;
-        }
-         return isPowerOfTwo(n / 2);
-
-        
+       if(n<=0)return false; 
+    return (n&(n-1))==0? true:false;
     }
-}
+    }
