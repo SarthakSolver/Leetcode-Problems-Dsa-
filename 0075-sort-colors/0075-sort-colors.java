5f6314635f1c66e89e94c2f15029c0540e23b966
Sarthak Sahu <148975170+SarthakSolver@users.noreply.github.com>
2026-04-09 23:25:19 +0530

    Time: 0 ms (100%), Space: 43.2 MB (96.01%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,32 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count1=0;
-        int count2=0;
-        int count3=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                count1++;
+        int low=0,mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums,mid,high);
+                high--;
             }
         }
-         for(int i=0;i<nums.length;i++){
-            if(nums[i]==1){
-                count2++;
-            }
-        }
-         for(int i=0;i<nums.length;i++){
-            if(nums[i]==2){
-                count3++;
-            }
-        }
-         for(int i=0;i<count1;i++){
-           nums[i]=0;
-        }
-         for(int i=count1;i<count1+count2;i++){
-           nums[i]=1;
-        }
-         for(int i=count1+count2;i<count1+count2+count3;i++){
-           nums[i]=2;
-        }
-        
+    }
+    public int[] swap(int arr[],int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    
+    return arr;
     }
 }
